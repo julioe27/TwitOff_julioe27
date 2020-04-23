03e50a369e1bdf00f3ac35afa9984c8e545ee11f
Julio E <je27@protonmail.com>
2020-04-22 20:56:54 -0700

    Added html and templates

--- web_app/__init__.py
+++ web_app/__init__.py
@@ -1,24 +1,22 @@
 # web_app/__init__.py
 
 from flask import Flask
-from web_app.models imprt db.migrate
-from web_app.routes.admin_routes import admin_routes
-from web_app.routes.home_routes import home_routes
-from web_app.routes.book_routes import  book_routes
-from web_app.routes.twitter_routes import twitter_routes
-from web_app.routes.stats_routes import stats_routes
 
-DATABASE_URI = "sqlite:///C:\Users\julio\Desktop\Lambda\TwitOff_julioe27\twitoff_development.db"
+from web_app.models import db, migrate
+from web_app.routes.home_routes import home_routes
+from web_app.routes.book_routes import book_routes
+
+DATABASE_URI = "sqlite:///C:\\Users\\julio\\Desktop\\Lambda\\TwitOff_julioe27\\twitoff_development.db"
 SECRET_KEY = "super secret"
 
 def create_app():
-    app=Flask(__name__)
-    app.config["SECRET_KYE"] = SECRET_KEY
+    app = Flask(__name__)
+    app.config["SECRET_KEY"] = SECRET_KEY # enable flash messaging via sessions
 
-    app.config['SQLALCHEMY_DATABASSE_URI'] =DATABASE_URI
+    app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URI
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.init_app(app)
-    migriate.init_app(app,db)
+    migrate.init_app(app, db)
 
     app.register_blueprint(home_routes)
     app.register_blueprint(book_routes)
--- web_app/routes/homes_routes.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask
-
-homes_routes = Blueprint("home_routes",__name__)
-
-@homes_routes.route("/route")
-def about():
-    return "About Me"
