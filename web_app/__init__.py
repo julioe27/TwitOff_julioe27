13e2f68a3a84a434a2c10b60af56670e17e4e47c
Julio E <je27@protonmail.com>
2020-04-22 20:21:14 -0700

    Added html and templates

--- web_app/__init__.py
+++ web_app/__init__.py
@@ -1,12 +1,28 @@
-from flask import Flask
+# web_app/__init__.py
 
+from flask import Flask
+from web_app.models imprt db.migrate
+from web_app.routes.admin_routes import admin_routes
 from web_app.routes.home_routes import home_routes
-# from web_app.routes.book_routes import book_routes
+from web_app.routes.book_routes import  book_routes
+from web_app.routes.twitter_routes import twitter_routes
+from web_app.routes.stats_routes import stats_routes
+
+DATABASE_URI = "sqlite:///C:\Users\julio\Desktop\Lambda\TwitOff_julioe27\twitoff_development.db"
+SECRET_KEY = "super secret"
 
 def create_app():
-    app = Flask(__name__)
+    app=Flask(__name__)
+    app.config["SECRET_KYE"] = SECRET_KEY
+
+    app.config['SQLALCHEMY_DATABASSE_URI'] =DATABASE_URI
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    db.init_app(app)
+    migriate.init_app(app,db)
+
     app.register_blueprint(home_routes)
-    # app.register_blueprint(book_routes)
+    app.register_blueprint(book_routes)
+
     return app
 
 if __name__ == "__main__":
--- web_app/routes/homes_routes.py
+++ web_app/routes/homes_routes.py
@@ -1,13 +1,7 @@
 from flask import Flask
 
-app = Flask(__name__)
-
-@homes_routes.route("/") # registers the route and defines what should when someone visits this routes
-def index():
-    x = 2 + 2
-    print("YOU VISTED THE HOMEPAGE")
-    return f"Hello World! {x}"
+homes_routes = Blueprint("home_routes",__name__)
 
 @homes_routes.route("/route")
 def about():
-    return "About Me (TODO)"
+    return "About Me"
